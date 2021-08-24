3a54fa5b28dbe2a199b9375e1e352f6475475299
Vee Satayamas <5ssgdxltv@relay.firefox.com>
2021-08-24 23:46:03 +0700

    Simplify types and support partially ambiguous lexical unit

--- src/lib.rs
+++ src/lib.rs
@@ -1,55 +1,44 @@
-//use nom::sequence::preceded;
-//use nom::character::complete::digit1;
-
-use nom::character::complete::alpha1;
+use nom::bytes::complete::escaped_transform;
+use nom::bytes::complete::is_not;
+use nom::bytes::complete::tag;
 use nom::character::complete::char;
+use nom::character::complete::one_of;
 use nom::character::complete::space1;
-use nom::character::complete::alphanumeric1;
 use nom::multi::separated_list0;
 use nom::sequence::delimited;
 use nom::IResult;
-use nom::bytes::complete::take_till1;
-use nom::bytes::complete::escaped;
-use nom::bytes::complete::escaped_transform;
-use nom::character::complete::one_of;
-use nom::bytes::complete::is_not;
-use nom::bytes::complete::tag;
-use nom::combinator::cut;
 
 #[derive(Debug, PartialEq)]
 pub enum StreamUnit {
-    LexicalUnit(LexicalUnit),
+    LexicalUnit(Vec<Analysis>),
     Chunk(String),
 }
 
 #[derive(Debug, PartialEq)]
-pub enum Analysis {
-    SurfaceForm(String),
-    Lemma(String),
+pub struct Analysis {
+    ling_form: String,
+    tags: Vec<String>,
 }
 
-#[derive(Debug, PartialEq)]
-pub struct LexicalUnit {
-    pub analyses: Vec<Analysis>,
-}
-
-pub fn parse_surface_form(input: &str) -> IResult<&str, Analysis> {
-    let inner_parse = is_not(r#"^$/<>{}\"#);
-    let mut parse = escaped_transform(inner_parse, '\\', one_of("^$"));
-    parse(input).map(|(i, o)| (i, Analysis::SurfaceForm(o.to_owned())))
+pub fn parse_analysis(input: &str) -> IResult<&str, Analysis> {
+    let lemma_inner_parse = is_not(r#"^$/<>{}\"#);
+    let mut lemma_escape_parse = escaped_transform(lemma_inner_parse, '\\', one_of("^$"));
+    lemma_escape_parse(input).map(|(i, o)| {
+        (
+            i,
+            Analysis {
+                ling_form: o.to_string(),
+                tags: vec![],
+            },
+        )
+    })
 }
 
 pub fn parse_lu(input: &str) -> IResult<&str, StreamUnit> {
-    let mut parse = delimited(char('^'), parse_surface_form, char('$'));
+    let parse_analyses = separated_list0(tag("/"), parse_analysis);
+    let mut parse = delimited(char('^'), parse_analyses, char('$'));
     let res = parse(input);
-    res.map(|(i, o)| {
-        (
-            i,
-            StreamUnit::LexicalUnit(LexicalUnit {
-                analyses: vec![o],
-            }),
-        )
-    })
+    res.map(|(i, o)| (i, StreamUnit::LexicalUnit(o)))
 }
 
 pub fn parse_stream(input: &str) -> IResult<&str, Vec<StreamUnit>> {
@@ -67,9 +56,10 @@ mod tests {
             parse_lu("^กา$"),
             Ok((
                 "",
-                StreamUnit::LexicalUnit(LexicalUnit {
-                    analyses: vec![Analysis::SurfaceForm(String::from("กา"))]
-                })
+                StreamUnit::LexicalUnit(vec![Analysis {
+                    ling_form: String::from("กา"),
+                    tags: vec![]
+                }])
             ))
         );
     }
@@ -80,9 +70,30 @@ mod tests {
             parse_lu("^\\^ab\\$$"),
             Ok((
                 "",
-                StreamUnit::LexicalUnit(LexicalUnit {
-                    analyses: vec![Analysis::SurfaceForm(String::from("^ab$"))]
-                })
+                StreamUnit::LexicalUnit(vec![Analysis {
+                    ling_form: String::from("^ab$"),
+                    tags: vec![]
+                }])
+            ))
+        );
+    }
+
+    #[test]
+    fn ambiguous_lu() {
+        assert_eq!(
+            parse_lu("^ab/xy$"),
+            Ok((
+                "",
+                StreamUnit::LexicalUnit(vec![
+                    Analysis {
+                        ling_form: String::from("ab"),
+                        tags: vec![]
+                    },
+                    Analysis {
+                        ling_form: String::from("xy"),
+                        tags: vec![]
+                    }
+                ])
             ))
         );
     }
@@ -93,9 +104,10 @@ mod tests {
             parse_stream("^ab$"),
             Ok((
                 "",
-                vec![StreamUnit::LexicalUnit(LexicalUnit {
-                    analyses: vec![Analysis::SurfaceForm(String::from("ab"))]
-                })]
+                vec![StreamUnit::LexicalUnit(vec![Analysis {
+                    ling_form: String::from("ab"),
+                    tags: vec![]
+                }])]
             ))
         );
         assert_eq!(
@@ -103,12 +115,14 @@ mod tests {
             Ok((
                 "",
                 vec![
-                    StreamUnit::LexicalUnit(LexicalUnit {
-                        analyses: vec![Analysis::SurfaceForm(String::from("ab"))]
-                    }),
-                    StreamUnit::LexicalUnit(LexicalUnit {
-                        analyses: vec![Analysis::SurfaceForm(String::from("cd"))]
-                    })
+                    StreamUnit::LexicalUnit(vec![Analysis {
+                        ling_form: String::from("ab"),
+                        tags: vec![]
+                    }]),
+                    StreamUnit::LexicalUnit(vec![Analysis {
+                        ling_form: String::from("cd"),
+                        tags: vec![]
+                    }])
                 ]
             ))
         );
